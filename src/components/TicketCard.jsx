645e9e4c60e955fe2a0de231569ce2890eb04e27
grontho69 <mahathirm880@gmail.com>
2025-12-30 01:48:13 +0600

    fifth commit

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -1,3 +1,5 @@
+import { GoDotFill } from "react-icons/go";
+import { FaCalendarAlt } from "react-icons/fa";
 const TicketCard = ({ ticket, onClick }) => {
   return (
     <div
@@ -6,7 +8,7 @@ const TicketCard = ({ ticket, onClick }) => {
     >
       <div className="flex justify-between">
         <h3 className="font-semibold">{ticket.title}</h3>
-        <span className="text-xs px-2 py-2 bg-green-100 text-green-600 rounded-2xl">
+        <span className="text-xs px-2 py-2 flex items-center bg-green-100 text-green-600 rounded-2xl"><GoDotFill />
           {ticket.status}
         </span>
       </div>
@@ -14,9 +16,9 @@ const TicketCard = ({ ticket, onClick }) => {
       <p className="text-sm text-gray-500 my-2">{ticket.description}</p>
 
       <div className="text-xs text-gray-400 flex items-center gap-6">
-        <span className={`font-medium p-1 ${ticket.priority=='High' ? 'bg-red-100 text-red-500':ticket.priority=='Medium' ? 'bg-yellow-100 text-yellow-500': 'bg-green-100 text-green-500'}`}>{ticket.priority}</span>
+        <span className={`font-medium px-2 py-1 rounded ${ticket.priority=='High' ? 'bg-red-100 text-red-500':ticket.priority=='Medium' ? 'bg-yellow-100 text-yellow-500': 'bg-green-100 text-green-500'}`}>{ticket.priority}</span>
         <span className="ml-4">{ticket.customer }</span>
-        <span className="ml-auto">{ticket.createdAt}</span>
+        <span className="ml-auto gap-2 flex" ><FaCalendarAlt />{ticket.createdAt}</span>
       </div>
     </div>
   );
